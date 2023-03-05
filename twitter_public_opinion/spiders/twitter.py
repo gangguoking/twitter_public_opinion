da4f65eb14136ba047cc224ee09c3f094cb81e34
HuHao81 <hh@popoo.io>
2023-03-05 21:21:04 +0800

    add twitter people, bybit, ftx, sunyuchen

--- twitter_public_opinion/spiders/twitter.py
+++ twitter_public_opinion/spiders/twitter.py
@@ -1,14 +1,17 @@
 import json
+import logging
 
 import scrapy
+
+from twitter_public_opinion import twitter_list
 
 
 class TwitterSpider(scrapy.Spider):
     name = "twitter"
     allowed_domains = ["twitter.com"]
-    cz_url = "https://api.twitter.com/graphql/73BM9FU1mPITScnhs6iXug/UserTweets?variables=%7B%22userId%22%3A%22902926941413453824%22%2C%22count%22%3A40%2C%22includePromotedContent%22%3Atrue%2C%22withQuickPromoteEligibilityTweetFields%22%3Atrue%2C%22withSuperFollowsUserFields%22%3Atrue%2C%22withDownvotePerspective%22%3Afalse%2C%22withReactionsMetadata%22%3Afalse%2C%22withReactionsPerspective%22%3Afalse%2C%22withSuperFollowsTweetFields%22%3Atrue%2C%22withVoice%22%3Atrue%2C%22withV2Timeline%22%3Atrue%7D&features=%7B%22responsive_web_twitter_blue_verified_badge_is_enabled%22%3Atrue%2C%22responsive_web_graphql_exclude_directive_enabled%22%3Afalse%2C%22verified_phone_label_enabled%22%3Afalse%2C%22responsive_web_graphql_timeline_navigation_enabled%22%3Atrue%2C%22responsive_web_graphql_skip_user_profile_image_extensions_enabled%22%3Afalse%2C%22tweetypie_unmention_optimization_enabled%22%3Atrue%2C%22vibe_api_enabled%22%3Atrue%2C%22responsive_web_edit_tweet_api_enabled%22%3Atrue%2C%22graphql_is_translatable_rweb_tweet_is_translatable_enabled%22%3Atrue%2C%22view_counts_everywhere_api_enabled%22%3Atrue%2C%22longform_notetweets_consumption_enabled%22%3Atrue%2C%22tweet_awards_web_tipping_enabled%22%3Afalse%2C%22freedom_of_speech_not_reach_fetch_enabled%22%3Afalse%2C%22standardized_nudges_misinfo%22%3Atrue%2C%22tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled%22%3Afalse%2C%22interactive_text_enabled%22%3Atrue%2C%22responsive_web_text_conversations_enabled%22%3Afalse%2C%22longform_notetweets_richtext_consumption_enabled%22%3Afalse%2C%22responsive_web_enhance_cards_enabled%22%3Afalse%7D"
-    start_urls = [cz_url]
-    header = {
+    url = "https://api.twitter.com/graphql/73BM9FU1mPITScnhs6iXug/UserTweets?variables={user_json}&features=%7B%22responsive_web_twitter_blue_verified_badge_is_enabled%22%3Atrue%2C%22responsive_web_graphql_exclude_directive_enabled%22%3Afalse%2C%22verified_phone_label_enabled%22%3Afalse%2C%22responsive_web_graphql_timeline_navigation_enabled%22%3Atrue%2C%22responsive_web_graphql_skip_user_profile_image_extensions_enabled%22%3Afalse%2C%22tweetypie_unmention_optimization_enabled%22%3Atrue%2C%22vibe_api_enabled%22%3Atrue%2C%22responsive_web_edit_tweet_api_enabled%22%3Atrue%2C%22graphql_is_translatable_rweb_tweet_is_translatable_enabled%22%3Atrue%2C%22view_counts_everywhere_api_enabled%22%3Atrue%2C%22longform_notetweets_consumption_enabled%22%3Atrue%2C%22tweet_awards_web_tipping_enabled%22%3Afalse%2C%22freedom_of_speech_not_reach_fetch_enabled%22%3Afalse%2C%22standardized_nudges_misinfo%22%3Atrue%2C%22tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled%22%3Afalse%2C%22interactive_text_enabled%22%3Atrue%2C%22responsive_web_text_conversations_enabled%22%3Afalse%2C%22longform_notetweets_richtext_consumption_enabled%22%3Afalse%2C%22responsive_web_enhance_cards_enabled%22%3Afalse%7D"
+    start_urls = []
+    twitter_header = {
         'authority': 'api.twitter.com',
         'accept': '*/*',
         'accept-language': 'en,zh-CN;q=0.9,zh;q=0.8',
@@ -34,19 +37,36 @@ class TwitterSpider(scrapy.Spider):
         """
         :return:
         """
-        for url in self.start_urls:
+        for user in twitter_list.user_dict:
+            url = self.url.format(user_json=twitter_list.user_dict[user])
+            print(url)
             yield scrapy.FormRequest(url=url,
                                      method="get",
-                                     headers=self.header,
-                                     dont_filter=True)
+                                     headers=self.twitter_header,
+                                     dont_filter=True,
+                                     meta={"twitter_user": user})
             # break
 
     def parse(self, response):
+        # twitter_user
+        twitter_user = response.meta['twitter_user']
+        logging.info(twitter_user)
         json_data = json.loads(response.text)
-        print(json_data)
+        speech_list = json_data['data']['user']['result']['timeline_v2']['timeline']['instructions'][1]['entries']
+
+        # twitter comments
+        for row in speech_list:
+            try:
+                if 'tweet' in row['content']['itemContent']['tweet_results']['result']:
+                    logging.info(row['content']['itemContent']['tweet_results']['result']['tweet']['legacy']['full_text'])
+                else:
+                    logging.info(row['content']['itemContent']['tweet_results']['result']['legacy']['full_text'])
+            except Exception as exc:
+                logging.warning(exc)
+                continue
 
 
-# 直接调用scrapy，适合本地开发环境使用
+# use scrapy，local
 if __name__ == '__main__':
     from scrapy import cmdline
 
---
+++ twitter_public_opinion/twitter_list.py
@@ -0,0 +1,9 @@
+# twitter牛人id
+
+
+user_dict = {
+    "@Bybit_Official": "%7B%22userId%22%3A%22999947328621395968%22%2C%22count%22%3A40%2C%22includePromotedContent%22%3Atrue%2C%22withQuickPromoteEligibilityTweetFields%22%3Atrue%2C%22withSuperFollowsUserFields%22%3Atrue%2C%22withDownvotePerspective%22%3Afalse%2C%22withReactionsMetadata%22%3Afalse%2C%22withReactionsPerspective%22%3Afalse%2C%22withSuperFollowsTweetFields%22%3Atrue%2C%22withVoice%22%3Atrue%2C%22withV2Timeline%22%3Atrue%7D",
+    "@cz_binance": "%7B%22userId%22%3A%22902926941413453824%22%2C%22count%22%3A40%2C%22includePromotedContent%22%3Atrue%2C%22withQuickPromoteEligibilityTweetFields%22%3Atrue%2C%22withSuperFollowsUserFields%22%3Atrue%2C%22withDownvotePerspective%22%3Afalse%2C%22withReactionsMetadata%22%3Afalse%2C%22withReactionsPerspective%22%3Afalse%2C%22withSuperFollowsTweetFields%22%3Atrue%2C%22withVoice%22%3Atrue%2C%22withV2Timeline%22%3Atrue%7D",
+    "@SBF_FTX": "%7B%22userId%22%3A%221110877798820777986%22%2C%22count%22%3A40%2C%22includePromotedContent%22%3Atrue%2C%22withQuickPromoteEligibilityTweetFields%22%3Atrue%2C%22withSuperFollowsUserFields%22%3Atrue%2C%22withDownvotePerspective%22%3Afalse%2C%22withReactionsMetadata%22%3Afalse%2C%22withReactionsPerspective%22%3Afalse%2C%22withSuperFollowsTweetFields%22%3Atrue%2C%22withVoice%22%3Atrue%2C%22withV2Timeline%22%3Atrue%7D",
+    "@UncleSimonweb3": "%7B%22userId%22%3A%222934398172%22%2C%22count%22%3A40%2C%22includePromotedContent%22%3Atrue%2C%22withQuickPromoteEligibilityTweetFields%22%3Atrue%2C%22withSuperFollowsUserFields%22%3Atrue%2C%22withDownvotePerspective%22%3Afalse%2C%22withReactionsMetadata%22%3Afalse%2C%22withReactionsPerspective%22%3Afalse%2C%22withSuperFollowsTweetFields%22%3Atrue%2C%22withVoice%22%3Atrue%2C%22withV2Timeline%22%3Atrue%7D"
+}
